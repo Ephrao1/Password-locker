060d56d6d1890bf4485b873fa91c9cf3a19f31cd
Ephraim Bundi <ebundi04@gmail.com>
2021-09-06 13:59:39 +0300

    Modify social-test file

--- social_test.py
+++ social_test.py
@@ -15,7 +15,7 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.password, "password3031")
         self.assertEqual(self.new_credentials.email, "ebundi04@gmail.com")
 
-        def test_save_credentials(self):
+    def test_save_credentials(self):
         '''
         test_save_account test case to test if the account object is saved into
          the account list
@@ -28,7 +28,8 @@ class TestCredentials(unittest.TestCase):
             tearDown method that does clean up after each test case has run.
             '''
             Credentials.credentials_list = []
-            def test_save_multiple_credentials(self):
+            
+    def test_save_multiple_credentials(self):
         '''
             test_save_multiple_account to check if we can save multiple account
             objects to our account_list
@@ -55,5 +56,3 @@ class TestCredentials(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
- 
