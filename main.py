739cef5eefaaec244cf6073e5bb647bac971042d
Ephraim Bundi <ebundi04@gmail.com>
2021-09-06 13:32:42 +0300

    Add find user function

--- main.py
+++ main.py
@@ -9,9 +9,24 @@ def create_user(acc_name, user_name, password, email):
     new_user = User(acc_name, user_name, password, email)
     return new_user
 
-    
+
     def save_user(user):
     '''
     Function to save user
     '''
     user.save_user()
+
+
+    def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+
+
+    def find_user(name):
+    '''
+    Function that finds a user by name and returns the user
+    '''
+    return User.find_by_name(name)
+
