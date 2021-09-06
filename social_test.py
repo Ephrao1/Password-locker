6cb287b36550ae76d9e3f139755ca72655a2c866
Ephraim Bundi <ebundi04@gmail.com>
2021-09-06 13:56:30 +0300

    Add save credentials function

--- social_test.py
+++ social_test.py
@@ -14,3 +14,18 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.user_name, "Ephraim")
         self.assertEqual(self.new_credentials.password, "password3031")
         self.assertEqual(self.new_credentials.email, "ebundi04@gmail.com")
+
+        def test_save_credentials(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_credentials.save_credentials()  
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credentials.credentials_list = []
+ 
